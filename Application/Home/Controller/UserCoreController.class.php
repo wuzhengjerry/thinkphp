2164ab3611b7bf68f586b49ec897f382a13a527f
wuzhengjerry <wuzhengmail@163.com>
2018-04-11 09:51:47 +0800

    parent::

--- Application/Home/Controller/UserCoreController.class.php
+++ Application/Home/Controller/UserCoreController.class.php
@@ -11,6 +11,8 @@ class UserCoreController extends CoreController{
 
     /*核心继承*/
     protected function _initialize() {
+        //继承CoreController的初始化函数
+        parent::_initialize();
     	$AUTH_KEY = session(C('AUTN_KEY'));
     	//判断认证key如果小于1，跳转到后台登陆网关
     	if($AUTH_KEY < 1) {
@@ -19,5 +21,4 @@ class UserCoreController extends CoreController{
     }
 }
 
-//继承CoreController的初始化函数
-parent::_initialize();
+
